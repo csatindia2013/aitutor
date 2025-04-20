7de2c0e0691062735b2960658d1dbb91b6f09e9a
csatindia2013 <40131283+csatindia2013@users.noreply.github.com>
2025-04-21 01:49:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,4 +147,6 @@ def admin_panel():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
